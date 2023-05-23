81e6e380eca01c54d20fd27fc4a78a83a40a5744
Vinodraj Vk <112770361+VinodrajVK@users.noreply.github.com>
2023-05-23 23:43:15 +0530

    Update mergeex.c

--- mergeex.c
+++ mergeex.c
@@ -5,17 +5,37 @@
 #define y 100
 #define inc 10
 int count;
-void worstarr(int *a,int n)
+int join(int arr[],int left[],int right[],int l,int m,int r)
 {
-int i;
-srand(time(0));
-a[0]=rand()%100;
-for(i=0;i<n-2;i+=2)
+int i,j;
+for(i=0;i<=m-l;i++)
+arr[i]=left[i];
+for(j=0;j<r-m;j++)
+arr[i+j]=right[j];
+}
+
+int split(int arr[],int left[],int right[],int l,int m,int r)
 {
-a[i+1]=a[i]+rand()%100;
-a[i+2]=a[i]-rand()%100;
+for(int i=0;i<=m-l;i++)
+left[i]=arr[i*2];
+for(int j=0;j<r-m;j++)
+right[j]=arr[j*2+1];
+}
+
+int genWorstCase(int arr[],int l,int r)
+{
+if(l<r)
+{
+int m=l+(r-l)/2;
+int left[m-l+1];
+int right[r-m];
+split(arr,left,right,l,m,r);
+genWorstCase(left,l,m);
+genWorstCase(right,m+1,r);
+join(arr,left,right,l,m,r);
 }
 }
+
 void copy(int *a,int l, int r, int *b)
 {
 int i=l;
@@ -66,6 +86,7 @@ srand(time(0));
 for(n=x;n<=y;n+=inc)
 {
 a = (int*)malloc(n*sizeof(int));
+
 //Best Case
 a[0]=rand()%100;
 for(i=1;i<n;i++)
@@ -94,7 +115,7 @@ fprintf(fpa,"%d ",a[i]);
 fprintf(fpa,"\n");
 
 //Worst Case
-worstarr(a,n);
+genWorstCase(a,0,n-1);
 count=0;
 for(i=0;i<n;i++)
 fprintf(fpa,"%d ",a[i]);
