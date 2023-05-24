7c975588dcb6b779520c0e91904d66a2963ef928
Vinodraj <beanadi1910@gmail.com>
2023-05-24 12:48:05 +0530

    May 24

--- BinarySearchR.c
+++ BinarySearchR.c
@@ -4,58 +4,59 @@
 #define x 10
 #define y 100
 #define inc 10
-int count=0;
-int bs(int *a,int b,int e,int key)
+int count;
+int bs(int *a,int l,int r,int key)
 {
-int mid = (b+e)/2;
+if(l>r)
+return -1;
+int mid = (l+r)/2;
+count++;
 if(a[mid]==key)
 return mid;
-if(b>e)
-return -1;
 if(a[mid]>key)
-return bs(a,b,mid-1,key);
-return bs(a,mid+1,e,key);
+return binarysearch(a,l,mid-1,key);
+return binarysearch(a,mid+1,r,key);
 }
 void main()
 {
-FILE *fp1,*fp2,*fp3,*fp4;
+FILE *fp1,*fp2;
 int *a,n,i,key,pos;
 fp1 = fopen("bsearchr.txt","w");
-fp2 = fopen("bsearchrb.txt","w");
-fp3 = fopen("bsearchrw.txt","w");
-fp4 = fopen("bsearchra.txt","w");
+fp2 = fopen("bsearchrcount.txt","w");
 srand(time(0));
 for(n=x;n<=y;n+=inc)
 {
-count=0;
 a=(int *)malloc(n*sizeof(int));
 fprintf(fp1,"\nArray :");
-for(i=0;i<n;i++)
-{
-a[i]=rand()%100;
-fprintf(fp1," %d",a[i]);
-}
+a[0]=rand()%100;
+for(i=1;i<n;i++)
+a[i]=a[i-1]+rand()%100;
+
 //Best case
 key = a[(n-1)/2];
+count=0;
 fprintf(fp1,"\nKey best case : %d\n",key);
-pos=bs(a,0,n-1,key);
+pos=binarysearch(a,0,n-1,key);
 fprintf(fp1,"Best Case Position : %d\n",pos+1);
-fprintf(fp2,"%d\t%d\n",n,count);
+fprintf(fp2,"%d\t%d\t",n,count);
+
+//Average Case
+key = a[(n-1)/4];
+count=0;
+fprintf(fp1,"Key average case : %d\n",key);
+pos=binarysearch(a,0,n-1,key);
+fprintf(fp1,"Average Case Position : %d\n",pos+1);
+fprintf(fp2,"%d\t",count);
+
 //Worst case
 key = 9999;
+count=0;
 fprintf(fp1,"Key worst case : %d\n",key);
-pos=bs(a,0,n-1,key);
+pos=binarysearch(a,0,n-1,key);
 fprintf(fp1,"Worst Case Position : %d\n",pos+1);
-fprintf(fp3,"%d\t%d\n",n,count);
-//Average Case
-key = a[3*(n-1)/4];
-fprintf(fp1,"Key average case : %d\n",key);
-pos=bs(a,0,n-1,key);
-fprintf(fp1,"Average Case Position : %d\n",pos+1);
-fprintf(fp4,"%d\t%d\n",n,count);
+fprintf(fp2,"%d\n",count);
 }
 fclose(fp1);
 fclose(fp2);
-fclose(fp3);
-fclose(fp4);
+system("gedit bsearchr.txt bsearchrcount.txt");
 }
