bc1b1e8da18d23d2ab9fa9c4ac124828eb90b7f5
Vinodraj Vk <112770361+VinodrajVK@users.noreply.github.com>
2023-07-01 19:54:35 +0530

    Create Floyd.c

---
+++ Floyd.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+int i, j, k, n, count;
+int minimum(int a, int b)
+{
+    return (a < b) ? a : b;
+}
+void copyarr(int c[n][n], int d[n][n])
+{
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
+            c[i][j] = d[i][j];
+}
+void floyd(int a[n][n])
+{
+    int p[n][n], q[n][n];
+    copyarr(p, a);
+    for (k = 1; k <= n; k++)
+    {
+        for (i = 1; i <= n; i++)
+            for (j = 1; j <= n; j++)
+            {
+                count++;
+                q[i][j] = minimum(p[i][j], (p[i][k] + p[k][j]));
+            }
+
+        copyarr(p, q);
+    }
+    printf("Distance Matrix\n");
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+            printf("%d ", q[i][j]);
+        printf("\n");
+    }
+}
+int main()
+{
+    printf("Enter the number of nodes : ");
+    scanf("%d", &n);
+    int a[n][n];
+    printf("Enter the Weight matrix\n");
+    printf("!!Note : Enter 999 for Infinity!!\n");
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
+            scanf("%d", &a[i][j]);
+    floyd(a);
+    printf("Operation Count : %d\n", count);
+    return 0;
+}
