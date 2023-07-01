dc5fdaae0ea6fa4c7d0d375ae9686f1956c4c887
Vinodraj Vk <112770361+VinodrajVK@users.noreply.github.com>
2023-07-01 10:49:09 +0530

    Update HeapSort.c

--- HeapSort.c
+++ HeapSort.c
@@ -1,91 +1,76 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-#define x 10
-#define y 100
-#define inc 10
-int count, countc;
-void heapify(int H[], int n)
-{
-    int i, j, k, v, heap;
-    for (i = n / 2; i >= 1; i--)
-    {
-        k = i;
-        v = H[k];
-        heap = 0;
-        while (!heap && 2 * k <= n)
-        {
-            // countc++;
-            j = 2 * k;
-            if (j < n)
-            {
-
-                if (H[j] < H[j + 1])
-                {
-                    countc++;
-                    j++;
-                }
-            }
-            if (v >= H[j])
-            {
-                // countc++;
-                heap = 1;
-            }
-            else
-            {
-                // countc++;
-                H[k] = H[j];
-                k = j;
-            }
-            H[k] = v;
-        }
-    }
-}
+#include <time.h>
+int count = 0;
 void swap(int *a, int *b)
 {
-    int t = *a;
+    int temp = *a;
     *a = *b;
-    *b = t;
+    *b = temp;
 }
-void heapsort(int H[], int n)
+void heapify(int a[], int n, int i)
 {
-    for (int i = n; i >= 2; i--)
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+
+    count++;
+    if (left < n && a[left] > a[largest])
+        largest = left;
+
+    if (right < n && a[right] > a[largest])
+        largest = right;
+
+    if (largest != i)
     {
-        count++;
-        swap((H + 1), (H + n));
-        // heapsort(H, n - 1);
-        heapify(H, n - 1);
+        swap(a + i, a + largest);
+        heapify(a, n, largest);
     }
 }
+
+void heapsort(int a[], int n)
+{
+    int i;
+    for (i = n / 2; i >= 1; i--)
+        heapify(a, n, i);
+    heapify(a, n, 1);
+    for (i = n - 1; i >= 2; i--)
+    {
+        swap(a + 1, a + i);
+        heapify(a, i, 1);
+    }
+}
+
 int main()
 {
-    int *H, i, n;
-    FILE *fp, *fpc;
-    fp = fopen("HSort.txt", "w");
-    fpc = fopen("HSortCount.txt", "w");
+    int *a, i, n;
     srand(time(0));
-    for (n = x; n <= y; n += inc)
+    FILE *fpc = fopen("HSortCount.txt", "w");
+    for (n = 10; n <= 100; n += 10)
     {
-        H = (int *)malloc((n + 1) * sizeof(int));
-        fprintf(fp, "\nArray : ");
-        H[1] = rand() % 100;
-        fprintf(fp, " %d", H[1]);
-        for (i = 2; i <= n; i++)
-        {
-            H[i] = H[i - 1] + rand() % 100;
-            fprintf(fp, " %d", H[i]);
-        }
+        a = (int *)malloc((n + 1) * sizeof(int));
+        // Best case
         count = 0;
-        countc = 0;
-        heapify(H, n);
-        heapsort(H, n);
-        fprintf(fp, "\nSorted Array : ");
         for (i = 1; i <= n; i++)
-            fprintf(fp, " %d", H[i]);
-        printf("%d\t%d\n", n, (countc));
-        fprintf(fpc, "%d\t%d\n", n, (countc));
+            a[i] = n;
+        heapsort(a, n);
+        fprintf(fpc, "%d\t%d\t", n, count);
+
+        // Average Case
+        for (i = 1; i <= n; i++)
+            a[i] = rand() % 100;
+        count = 0;
+        heapsort(a, n);
+        fprintf(fpc, "%d\t", count);
+
+        // Worst case
+        for (i = 1; i <= n; i++)
+            a[i] = i;
+        count = 0;
+        heapsort(a, n);
+        fprintf(fpc, "%d\n", count);
     }
-    fclose(fp);
     fclose(fpc);
+    free(a);
     return 0;
 }
