053acfc8b7379d08416924f75eac4d138073f929
Vinodraj Vk <112770361+VinodrajVK@users.noreply.github.com>
2023-07-18 19:35:22 +0530

    Update BFS.c

--- BFS.c
+++ BFS.c
@@ -1,47 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 int i, n, j, count = 0, opc, visit[10], acyclic = 1, f = 0, r = -1;
 int a[10][10], traverse[10][10], queue[10];
 void bfs(int s)
 {
-    int k = 1;
+    int k = 1, p[n];
     count++;
     queue[++r] = s;
+    p[s] = 0;
+    visit[s] = 0;
     while (f <= r)
     {
         s = queue[f++];
         visit[s] = 1;
         traverse[count][k++] = s;
-        for (i = s; i <= n; i++)
+        for (i = 1; i <= n; i++)
         {
             opc++;
-            if (a[s][i] && (visit[i] == 1))
-            {
-                opc++;
+            if (a[s][i] && visit[i] == 1 && p[s] != i)
                 acyclic = 0;
-            }
-            if (a[s][i] && visit[i] == -1)
+            if (a[s][i] && visit[i] == 0)
             {
-                opc++;
                 queue[++r] = i;
-                visit[i] = 0;
+                p[i] = s;
             }
         }
     }
 }
 void connectandcyclic()
 {
-    int f = 1;
+    int flag = 1;
     for (int i = 1; i <= n; i++)
     {
         if (visit[i] == 1)
             visit[i] = 2;
-        if (visit[i] == -1)
+        if (visit[i] == 0)
         {
-            f = 0;
+            flag = 0;
             bfs(i);
         }
     }
-    if (f)
+    if (flag)
     {
         printf("Graph is Connected\n");
         for (j = 1; j <= n; j++)
@@ -55,7 +54,7 @@ void connectandcyclic()
         for (i = 1; i <= count; i++)
         {
             printf("Component-%d : ", i);
-            for (j = 1; j < n; j++)
+            for (j = 1; j <= n; j++)
                 if (traverse[i][j] != 0)
                     printf("%d ", traverse[i][j]);
             printf("\n");
@@ -66,20 +65,21 @@ void connectandcyclic()
     else
         printf("\nGraph is Cyclic\n");
 }
-
 int main()
 {
     int start;
     printf("Enter the number of nodes : ");
     scanf("%d", &n);
     for (i = 1; i <= n; i++)
-        visit[i] = -1;
+        visit[i] = 0;
     printf("Enter the adjacency matrix\n");
     for (i = 1; i <= n; i++)
         for (j = 1; j <= n; j++)
             scanf("%d", &a[i][j]);
     printf("Enter the Starting Vertex : ");
     scanf("%d", &start);
+    opc = 0;
+    count = 0;
     bfs(start);
     connectandcyclic();
     printf("Operation Count : %d\n", opc);
