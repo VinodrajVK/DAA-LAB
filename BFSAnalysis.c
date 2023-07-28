7e273ded87209f79c9f1dd8787fb9cbfe138839a
Vinodraj Vk <112770361+VinodrajVK@users.noreply.github.com>
2023-07-28 19:29:16 +0530

    Create BFSAnalysis.c

---
+++ BFSAnalysis.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+int i, n, j, opc, visit[10], acyclic = 1, f = 0, r = -1;
+int a[10][10], queue[10];
+void bfs(int s)
+{
+    int k = 1, p[n];
+    queue[++r] = s;
+    p[s] = 0;
+    visit[s] = 0;
+    while (f <= r)
+    {
+        s = queue[f++];
+        visit[s] = 1;
+        for (i = 1; i <= n; i++)
+        {
+            opc++;
+            if (a[s][i] && visit[i] == 1 && p[s] != i)
+                acyclic = 0;
+            if (a[s][i] && visit[i] == -1)
+            {
+                queue[++r] = i;
+                visit[i] = 0;
+                p[i] = s;
+            }
+        }
+    }
+}
+void connectandcyclic()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (visit[i] == 1)
+            visit[i] = 2;
+        if (visit[i] == -1)
+            bfs(i);
+    }
+}
+int main()
+{
+    FILE *fp = fopen("BFSCount.txt", "w");
+    for (n = 4; n <= 6; n++)
+    {
+        opc = 0;
+        for (i = 1; i <= n; i++)
+            visit[i] = -1;
+        printf("Enter adjacency matrix for %d vertices\n", n);
+        for (i = 1; i <= n; i++)
+            for (j = 1; j <= n; j++)
+                scanf("%d", &a[i][j]);
+        bfs(1);
+        connectandcyclic();
+        fprintf(fp, "%d\t%d\n", n, opc);
+    }
+    fclose(fp);
+}
