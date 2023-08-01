97b0fc44a093464c38a7d3675f6a90e8154b8885
Vinodraj Vk <112770361+VinodrajVK@users.noreply.github.com>
2023-08-01 19:49:40 +0530

    Update BFS.c

--- BFS.c
+++ BFS.c
@@ -1,88 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
-int i, n, j, count = 0, opc, visit[10], acyclic = 1, f = 0, r = -1;
-int a[10][10], traverse[10][10], queue[10];
-void bfs(int s)
+int a[10][10], visit[10], n, Q[20], f = 0, r = -1, traverse[10][10], count = 0, opc = 0, cyclic = 0;
+void bfs(int start)
 {
-    int k = 1, p[n];
     count++;
-    queue[++r] = s;
-    p[s] = 0;
-    visit[s] = 0;
+    int p[n], v, k = 1, flag;
+    Q[++r] = start;
+    p[start] = 0;
     while (f <= r)
     {
-        s = queue[f++];
-        visit[s] = 1;
-        traverse[count][k++] = s;
-        for (i = 1; i <= n; i++)
+        flag = 0;
+        v = Q[f++];
+        traverse[count][k++] = v;
+        visit[v] = 1;
+        for (int i = 1; i <= n; i++)
         {
             opc++;
-            if (a[s][i] && visit[i] == 1 && p[s] != i)
-                acyclic = 0;
-            if (a[s][i] && visit[i] == -1)
+            if (a[v][i])
+                flag = 1;
+            if (a[v][i] && visit[i] == 1 && i != p[v])
+                cyclic = 1;
+            if (a[v][i] && visit[i] == -1)
             {
-                queue[++r] = i;
+                Q[++r] = i;
                 visit[i] = 0;
-                p[i] = s;
+                p[i] = v;
             }
         }
+        if (flag == 0)
+            visit[v] = 2;
     }
 }
-void connectandcyclic()
+void connectcyclic()
 {
-    int flag = 1;
+    int flag = 0;
     for (int i = 1; i <= n; i++)
     {
         if (visit[i] == 1)
             visit[i] = 2;
-        if (visit[i] == 0)
+        if (visit[i] == -1)
         {
-            flag = 0;
             bfs(i);
+            flag = 1;
+            break;
         }
     }
     if (flag)
     {
-        printf("Graph is Connected\n");
-        for (j = 1; j <= n; j++)
-            printf("%d ", traverse[1][j]);
-    }
-    else
-    {
-        printf("Graph is Disconnected\n");
-        printf("Number of Connected Components : %d\n", count);
-        printf("They are\n");
-        for (i = 1; i <= count; i++)
+        printf("The Graph is Disconnected\n");
+        printf("The Graph has %d connected Components\n", count);
+        printf("The Connected Components are\n");
+        for (int i = 1; i <= count; i++)
         {
-            printf("Component-%d : ", i);
-            for (j = 1; j <= n; j++)
+            for (int j = 1; j <= n; j++)
+            {
                 if (traverse[i][j] != 0)
                     printf("%d ", traverse[i][j]);
+            }
             printf("\n");
         }
     }
-    if (acyclic)
-        printf("\nGraph is Acyclic\n");
     else
-        printf("\nGraph is Cyclic\n");
+    {
+        printf("The Graph is Connected\n");
+        printf("The BFS Traversal is : ");
+        for (int j = 1; j <= n; j++)
+        {
+            if (traverse[1][j] != 0)
+                printf("%d ", traverse[1][j]);
+        }
+        printf("\n");
+    }
+    if (cyclic)
+        printf("Graph is Cyclic\n");
+    else
+        printf("Graph is Acyclic\n");
 }
-int main()
+void main()
 {
     int start;
-    printf("Enter the number of nodes : ");
+    printf("Enter the number of vertices: ");
     scanf("%d", &n);
-    for (i = 1; i <= n; i++)
+    printf("Enter the adjacency Matrix\n");
+    for (int i = 1; i <= n; i++)
+    {
         visit[i] = -1;
-    printf("Enter the adjacency matrix\n");
-    for (i = 1; i <= n; i++)
-        for (j = 1; j <= n; j++)
+        for (int j = 1; j <= n; j++)
+        {
             scanf("%d", &a[i][j]);
+            traverse[i][j] = 0;
+        }
+    }
     printf("Enter the Starting Vertex : ");
     scanf("%d", &start);
-    opc = 0;
-    count = 0;
     bfs(start);
-    connectandcyclic();
+    connectcyclic();
     printf("Operation Count : %d\n", opc);
-    return 0;
 }
